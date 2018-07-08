fcd02a94553b8f01accbaa97506550f163f3ca18
Yazgylych Rahymov <y.rahymov@gmail.com>
2018-07-08 15:39:16 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,7 +3,7 @@
 def display_board(board)
   puts "  #{board[0]} | #{board[1]}  |  #{board[2] "
   puts "-----------"
-  puts "  #{board[2]} | #{board[1]}  |  #{board[2] "
+  puts "  #{board[3]} | #{board[1]}  |  #{board[2] "
   puts "-----------"
   puts "   |   |   "
 end
