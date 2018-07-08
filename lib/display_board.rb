52223dce1d8abd89cc282c7a5dc6e0c5e970abd7
Yazgylych Rahymov <y.rahymov@gmail.com>
2018-07-08 15:46:15 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -5,6 +5,6 @@ def display_board(board)
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
-  puts " #{board[7]} | #{board[8]} | #{board[9]} "
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 board = ["X"," "," "," ","X"," "," "," ","X"]
