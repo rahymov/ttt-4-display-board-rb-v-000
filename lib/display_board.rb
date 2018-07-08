52a27f2409eeeb53bb1523cea2915bbb6f6ad4c9
Yazgylych Rahymov <y.rahymov@gmail.com>
2018-07-08 15:38:31 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,9 +1,9 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 def display_board(board)
-  puts "  #{board[0]} | #{board[1]}  |  #{board[2] \n"
+  puts "  #{board[0]} | #{board[1]}  |  #{board[2] "
   puts "-----------"
-  puts "   |   |   \n"
+  puts "   |   |   "
   puts "-----------\n"
   puts "   |   |   \n"
 end
